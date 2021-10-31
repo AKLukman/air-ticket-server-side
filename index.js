8d76912f0953a23dc96fc87e09cbc403a7b25b23
AKLukman <abulkashemlukman@gmail.com>
2021-10-31 22:25:04 +0600

    jjs

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const ObjectId = require('mongodb').ObjectId;
 
 require('dotenv').config();
 
-const port = process.env.PORT || 5050;
+const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
@@ -79,11 +79,11 @@ async function run() {
 run().catch(console.dir);
 
 app.get('/', (req, res) => {
-    res.send('server connected');
+    res.send('server Running');
 });
 
 
 
 app.listen(port, () => {
-    console.log('connected on port', port);
+    console.log('Connected on port', port);
 })
