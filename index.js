a46902a4e44898b358ab287c45c4b4ef1452133e
AKLukman <abulkashemlukman@gmail.com>
2022-06-28 23:01:39 +0600

    commit

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ app.use(express.json());
 // const a = process.env.DB_USER;
 
 
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.tdvhb.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.q3gzb.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
 async function run() {
